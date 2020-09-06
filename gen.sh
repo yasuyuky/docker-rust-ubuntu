387ac470e4ea5ab2bc02f715ad4c4587264955eb
yasuyuky <yasuyuki.ymd@gmail.com>
2020-09-06 20:55:28 +0900

    Move location of Dockerfile for auto-update

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eux
 
-VERSION=$(grep rust Dockerfile.official | cut -f 2 -d ':' | cut -f 1 -d '-')
+VERSION=$(grep FROM official/Dockerfile | cut -f 2 -d ':' | cut -f 1 -d '-')
 sed "s/%%RUST_VERSION%%/${VERSION}/g" Dockerfile.template >Dockerfile
 echo ::set-output name=version::${VERSION}
