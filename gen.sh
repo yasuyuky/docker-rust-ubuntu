441a963b9ea36d4c0271e9f8bfb6f1a89f79722e
κeen <3han5chou7@gmail.com>
2022-07-26 19:32:50 +0900

    build images for lts images other than bionic

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eux
-
+INPUT="$1"
+OUTPUT="${1%.template}"
 VERSION=$(grep FROM official/Dockerfile | cut -f 2 -d ':' | cut -f 1 -d '-')
-sed "s/%%RUST_VERSION%%/${VERSION}/g" Dockerfile.template >Dockerfile
+sed "s/%%RUST_VERSION%%/${VERSION}/g" "$INPUT" > "$OUTPUT"
 echo ::set-output name=version::${VERSION}
