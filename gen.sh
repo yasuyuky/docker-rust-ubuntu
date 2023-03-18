80fba87cc161c70f5da8053573c0d262bc6f57ac
yasuyuky <yasuyuki.ymd@gmail.com>
2023-03-18 20:43:31 +0900

    Refactor gen.sh script to set a default value for VERSION variable if it's not already set

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eux
-VERSION=$(grep FROM official/Dockerfile | cut -f 2 -d ':' | cut -f 1 -d '-')
+VERSION=${VERSION:-$(grep FROM official/Dockerfile | cut -f 2 -d ':' | cut -f 1 -d '-')}
 sed "s/%%RUST_VERSION%%/${VERSION}/g" Dockerfile.template >Dockerfile
 echo version=${VERSION} >>$GITHUB_OUTPUT
